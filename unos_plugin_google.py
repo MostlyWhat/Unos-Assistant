4cfb9db86d7db745cb497c0a31902c88f4be21af
MostlyWhat <mostlywhatstudios@gmail.com>
2021-11-08 18:48:40 +0700

    Updated Google Based Plugin

--- unos_plugin_google.py
+++ unos_plugin_google.py
@@ -1,6 +1,5 @@
 #Modules Importer
 from ssl import ALERT_DESCRIPTION_UNKNOWN_PSK_IDENTITY
-from google.cloud import speech as record
 import pyttsx3
 import sys
 from PyQt5 import QtWidgets
@@ -9,6 +8,12 @@ import time
 import os
 import random
 import json
+from __future__ import division
+import re
+import sys
+from google.cloud import speech
+import pyaudio
+from six.moves import queue
 
 
 class UNOS:
@@ -19,9 +24,14 @@ class UNOS:
     def UNOSinitialize(self):
         #Make Variables Global
         global speech
+        global client
+        global config
+        global streaming_config
         global recognizer
         global mic
         global config
+        global RATE
+        global CHUNK
         global API_KEY
         global GOOGLE_CLOUD_SPEECH_CREDENTIALS
         global WAKE
@@ -32,6 +42,10 @@ class UNOS:
 
         #Main Configurations
         config = dict(language_code="en-US")
+
+        # Audio Recording Parameters
+        RATE = 16000
+        CHUNK = int(RATE / 10)  # 100ms
 
         #Start Voice Commands
         WAKE = "hey uno's"
@@ -44,7 +58,18 @@ class UNOS:
 
         #Initilisation of Recognition Systems
         speech = pyttsx3.init()
-        recognizer = record.SpeechClient()
+        language_code = "en-US"  # a BCP-47 language tag
+
+        client = speech.SpeechClient()
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=language_code,
+        )
+
+        streaming_config = speech.StreamingRecognitionConfig(
+            config=config, interim_results=True
+        )
 
     #Verification Before Launch
     def Verify(self):
@@ -126,26 +151,31 @@ class UNOS:
         sys.exit(qapp.exec_())
 
     def RecognizeUNOS(self):
-        user_response = recognizer.recognize(config=config, audio=mic)
+        with MicrophoneStream(RATE, CHUNK) as stream:
+            audio_generator = stream.generator()
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
 
-        if WAKEUP_COMMANDS.count(user_response.lower()) > 0:
-            return "True"
+            responses = client.streaming_recognize(streaming_config, requests)
 
-        else:
-            return str(user_response.lower())
+            # Now, put the transcription responses to use.
+            for response in responses:
+            user_response = response.results[0]
  
-    def RecognizeAudio(self):
-        #Recognition of UNOS
-        try:
-            user_response = recognizer.recognize(config=config, audio=mic)
+    # def RecognizeAudio(self):
+    #     #Recognition of UNOS
+    #     try:
+    #         user_response = recognizer.recognize(config=config, audio=mic)
 
-            return str(user_response.lower())
+    #         return str(user_response.lower())
 
-        except record.UnknownValueError:
-            return "UnknownValueError"
+    #     except record.UnknownValueError:
+    #         return "UnknownValueError"
 
-        except record.RequestError as e:
-            return "RequestError"
+    #     except record.RequestError as e:
+    #         return "RequestError"
 
     def runningCommand(self):
         #Recognition of Voice Commands
@@ -165,3 +195,70 @@ class UNOS:
                 print("UNOS: Shutting Down System")
                 self.speak("shutting down system")
                 exit()
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
