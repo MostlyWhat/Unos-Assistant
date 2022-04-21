61f0bcf629388fb1181eaf29cbb320531d9fbd8f
MostlyWhat <mostlywhatstudios@gmail.com>
2022-04-21 20:56:08 +0700

    Updating Demo

--- unos.sh
+++ unos.sh
@@ -1,4 +1,7 @@
 echo "[ UNOS Assistant Framework ] Installing Dependencies"
+cd Demo
 pip3 install -r requirements.txt
+python3 setup.py
+cd ..
 echo "[ UNOS Assistant Framework ] Starting UNOS Assistant"
 python3 unos.py
