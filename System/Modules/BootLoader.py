f253784b7f240d29da85d93a60d4353ab5c5c9a2
MostlyWhat <mostlywhatstudios@gmail.com>
2022-04-21 20:19:08 +0700

    Updated Links to Files for BootLoader

--- System/Modules/BootLoader.py
+++ System/Modules/BootLoader.py
@@ -9,7 +9,7 @@ class Config:
         current_directory = Path(os.getcwd())
 
         # Launch Configuration
-        with open(f'{current_directory}\\Config\\launch_config.json') as launch_config_file:
+        with open('Config/launch_config.json') as launch_config_file:
             launch_data = json.load(launch_config_file)
 
             self.launch_mode = launch_data["launch_mode"]
@@ -17,7 +17,7 @@ class Config:
             self.dev_mode = launch_data["dev_mode"]
 
         # UNOS Configuration
-        with open(f'{current_directory}\\Config\\unos_config.json') as unos_config_file:
+        with open('Config/unos_config.json') as unos_config_file:
             unos_data = json.load(unos_config_file)
 
             self.unos_name = unos_data["name"]
@@ -27,7 +27,7 @@ class Config:
             self.unos_previous_interation = unos_data["previous_interation"]
 
         # Modules Configuration
-        with open(f'{current_directory}\\Config\\modules_config.json') as modules_config_file:
+        with open('Config/modules_config.json') as modules_config_file:
             modules_data = json.load(modules_config_file)
 
             self.modules_location = modules_data["system_modules"]["location"]
@@ -35,14 +35,14 @@ class Config:
             self.fallback_module = modules_data["system_modules"]["fallback_module"]
 
         # User Configuration
-        with open(f'{current_directory}\\Config\\user_config.json') as user_config_file:
+        with open('Config/user_config.json') as user_config_file:
             user_data = json.load(user_config_file)
 
             self.username = user_data["username"]
             self.date_of_birth = user_data["date_of_birth"]
 
         # Libaries Configuration
-        with open(f'{current_directory}\\Config\\libraries_config.json') as libraries_config_file:
+        with open('Config/libraries_config.json') as libraries_config_file:
             libraries_data = json.load(libraries_config_file)
 
             self.classes_lib = libraries_data["libraries"]["classes"]
@@ -51,33 +51,33 @@ class Config:
             self.default_dataset = libraries_data["dataset"]["default"]
 
         # MCAS Configuration
-        with open(f'{current_directory}\\Config\\MCAS_config.json') as MCAS_config_file:
+        with open('Config/MCAS_config.json') as MCAS_config_file:
             MCAS_data = json.load(MCAS_config_file)
 
             self.MCAS_core1_location = MCAS_data["Cores"]["core_1"]
             self.MCAS_core2_location = MCAS_data["Cores"]["core_2"]
             self.MCAS_core3_location = MCAS_data["Cores"]["core_3"]
 
-            with open(f'System\\Cores\\{self.MCAS_core1_location}') as core1_file:
+            with open(f'System/Cores/{self.MCAS_core1_location}') as core1_file:
                 MCAS_core1_data = json.load(core1_file)
 
-                self.MCAS_core1 = f'System\\Cores\\{MCAS_core1_data["info"]["name"]}\\{MCAS_core1_data["info"]["filename"]}'
+                self.MCAS_core1 = f'System/Cores/{MCAS_core1_data["info"]["name"]}/{MCAS_core1_data["info"]["filename"]}'
                 self.MCAS_core1_name = MCAS_core1_data["info"]["name"]
 
-            with open(f'System\\Cores\\{self.MCAS_core2_location}') as core2_file:
+            with open(f'System/Cores/{self.MCAS_core2_location}') as core2_file:
                 MCAS_core2_data = json.load(core2_file)
 
-                self.MCAS_core2 = f'System\\Cores\\{MCAS_core2_data["info"]["name"]}\\{MCAS_core2_data["info"]["filename"]}'
+                self.MCAS_core2 = f'System/Cores/{MCAS_core2_data["info"]["name"]}/{MCAS_core2_data["info"]["filename"]}'
                 self.MCAS_core2_name = MCAS_core2_data["info"]["name"]
 
-            with open(f'System\\Cores\\{self.MCAS_core3_location}') as core3_file:
+            with open(f'System/Cores/{self.MCAS_core3_location}') as core3_file:
                 MCAS_core3_data = json.load(core3_file)
 
-                self.MCAS_core3 = f'System\\Cores\\{MCAS_core3_data["info"]["name"]}\\{MCAS_core3_data["info"]["filename"]}'
+                self.MCAS_core3 = f'System/Cores/{MCAS_core3_data["info"]["name"]}/{MCAS_core3_data["info"]["filename"]}'
                 self.MCAS_core3_name = MCAS_core3_data["info"]["name"]
 
         # Credentials Configuration
-        with open(f'{current_directory}\\Config\\credentials_config.json') as credentials_config_file:
+        with open('Config/credentials_config.json') as credentials_config_file:
             credentials_data = json.load(credentials_config_file)
 
             # OpenWeatherMap Api
