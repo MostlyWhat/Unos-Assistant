d5043e6428c326f605ecacb36677105d5ee26334
MostlyWhat <mostlywhatstudios@gmail.com>
2022-06-01 13:35:07 +0700

    Fixed Minor Bugs in Speech

--- System/Modules/Speech.py
+++ System/Modules/Speech.py
@@ -47,27 +47,33 @@ if config.providers_recognition == "google" or config.providers_text_to_speech =
 class Speaker():
     def __init__(self):
         # init function to get an engine instance for the speech synthesis
-        if config.providers_text_to_speech == "pyttsx3":
-            self.engine = pyttsx3.init()
+        # if config.providers_text_to_speech == "pyttsx3":
+        #     self.engine = pyttsx3.init()
     
-        else:
-            self.engine = pyttsx3.init()
+        # else:
+        #     self.engine = pyttsx3.init()
+        self.engine = pyttsx3.init()
     
     def speak(self, text):
-        if config.providers_text_to_speech == "pyttsx3":
-            # say method on the engine that passing input text to be spoken
-            self.engine.say(text)
+        # if config.providers_text_to_speech == "pyttsx3":
+        #     # say method on the engine that passing input text to be spoken
+        #     self.engine.say(text)
 
-            # run and wait method, it processes the voice commands.
-            self.engine.runAndWait()
+        #     # run and wait method, it processes the voice commands.
+        #     self.engine.runAndWait()
     
-        else:
-            # say method on the engine that passing input text to be spoken
-            self.engine.say(text)
+        # else:
+        #     # say method on the engine that passing input text to be spoken
+        #     self.engine.say(text)
 
-            # run and wait method, it processes the voice commands.
-            self.engine.runAndWait()
+        #     # run and wait method, it processes the voice commands.
+        #     self.engine.runAndWait()
         
+        # say method on the engine that passing input text to be spoken
+        self.engine.say(text)
+
+        # run and wait method, it processes the voice commands.
+        self.engine.runAndWait()
 
 class Listener():  # Listener class to get the input from the user
     def __init__(self):
