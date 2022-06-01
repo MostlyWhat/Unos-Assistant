cc32c236654d8912293590e823494eecad4cb80b
MostlyWhat <mostlywhatstudios@gmail.com>
2022-06-01 13:30:13 +0700

    Update Manager.py

--- System/Analysis/Analyzers/Manager.py
+++ System/Analysis/Analyzers/Manager.py
@@ -6,18 +6,27 @@ updater = Updater()
 class Plugin:
     def __init__(self):
         self.name = "Manager"
-        self.contexts = ["update"]
+        self.contexts = ["update", "activate", "deactivate"]
 
     def analyze(self, query):
         return any((context in query for context in self.contexts))
 
     def process(self, query):
-        # Remove the first word from the query
-        command = query[7:]
+        if query.startswith("update"):
+            command = query.split(" ")[1]
+            return self.update(command)
 
+        elif query.startswith("activate"):
+            command = query.split(" ")[1]
+            return self.protocol(command)
+
+    def update(self, command):
         retrain = updater.regen(command)
         if retrain is True:
             return f"Sucessfully Regenerated the {command} Database"
 
         else:
             return f"Failed to Regenerate the {command} Database"
+
+    def protocol(self, command):
+        pass
